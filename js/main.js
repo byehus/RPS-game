5c69b331ab831b5e192ed63019fdf9bc627fd9a7
byehus <subbu.abbu@gmail.com>
2023-04-15 17:38:00 +0100

    Added game logic

--- js/main.js
+++ js/main.js
@@ -7,3 +7,29 @@ const result_div = document.querySelector(".result_");
 const rock_div = document.getElementById("roc_");
 const paper_div = document.getElementById("pap_");
 const scissors_div = document.getElementById("scis_");
+
+function game(userChoice) {
+    console.log("yes " + userChoice);
+}
+
+function cpuChoice() {
+    const choices = ["roc_", "pap_", "scis_"];
+    const randoNumber = Math.floor(Math.random() * 3);
+    return choices[randoNumber];
+}
+
+function main() {
+    rock_div.addEventListener("click", function () {
+        game("roc_");
+    });
+
+    paper_div.addEventListener("click", function () {
+        game("pap_");
+    });
+
+    scissors_div.addEventListener("click", function () {
+        game("scis_");
+    });
+}
+
+main();
